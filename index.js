21844e8b77d2bc1958e45c1c1d08650326e9d219
Larry <xiaozi7912@gmail.com>
2018-07-20 17:39:17 +0800

    Update.

--- index.js
+++ index.js
@@ -15,29 +15,7 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = FACEBOOK_PAGE_ACCESS_TOKEN;
-
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-
-        // Checks the mode and token sent is correct
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-            // Responds with the challenge token from the request
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-
-        } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
-            res.sendStatus(403);
-        }
-    }
+   res.sendStatus(200);
 });
 
 // Creates the endpoint for our webhook
